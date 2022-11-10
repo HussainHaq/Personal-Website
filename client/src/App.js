e4c1de4398a6be6308b3f9872a8560594006f345
Husain Haq <Hussainhaq16@gmail.com>
2022-11-09 23:32:22 -0600

    Update App.js
    updated App.js which pretty much adds a few new routes and protects the blog page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';
 //pages & components
 import Main from './components/main/main'
 import Navbar from './components/navbar/Navbar';
 import PostBlog from './components/pages/postBlog'
 import About from './components/pages/aboutPage';
-
-// This is running React-Router-dom v6
+import Footer from './components/footer/Footer';
+import Projects from './components/pages/Projects';
+import Login from './components/pages/Login'
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className='pages'>
           <Routes>
+            <Route path='/login' element={ user ? <Navigate to="/" /> :  <Login />} />
             <Route path='/' element={<Main />}/>  
-            <Route path='/blog_form' element={<PostBlog />}/>  
             <Route path='/about' element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            {/* Protected Route*/}
+            <Route path='/blog_form' element={user ? <PostBlog /> : <Main/> } />  
+            
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
